df66680b1192bfe57d12903e489ce0cf63d48601
Alejandro González Rodríguez <TheGoodGuyNano>
2023-02-12 14:18:14 -0600

    debug on vscode

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "stdint.h"
+
+int main()
+{
+    int i = 0;
+    printf("Hello World!\n");
+    for(i = 0; i < 3; i++)
+    {
+        printf("Nano\n");
+    }
+    return 0;     
+}
